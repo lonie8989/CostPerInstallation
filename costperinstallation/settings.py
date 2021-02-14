01315f0d81cbc2477d2a9712ba5f1e4e960bc99f
Ronnie <ckzks2013@gmail.com>
2021-02-14 01:02:20 -0800

    connected mysql

--- costperinstallation/settings.py
+++ costperinstallation/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'costperinstallation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'isolation_level': 'read committed',
+            'host': 'localhost',
+            'database': 'costperinstallation',
+            'user': 'root'
+        },
     }
 }
 
@@ -105,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Los_Angeles'
 
 USE_I18N = True
 
